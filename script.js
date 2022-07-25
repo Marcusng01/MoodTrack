cacf195cf396ed09be117e967cfa3a0c01c7d185
Marcusng01 <71885636+Marcusng01@users.noreply.github.com>
2022-07-25 17:32:03 +0800

    Mood Tracking (nosave)
    click on date to track mood. Doesn't work or save old moods when changing pages

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const day = ["Sun","Mon", "Tue", "Wed","Thu","Fri","Sat"];
 var months = ["January", "February", "March", "April", "May",
   "June", "July", "August", "September", "October", "November", "December"];
+var colors = ["rgb(241, 212, 212)","rgb(184, 9, 207)","rgb(86, 115, 248)",
+"rgb(253, 215, 75)","rgb(253, 66, 100)","rgb(58, 58, 68)","rgb(212, 212, 212)"];
 const d = new Date();
 
 var monthIndex = d.getMonth();
@@ -58,7 +60,6 @@ function currentDatesMiddle(month,year){
   }
 }
 currentDatesMiddle(monthIndex,year);
-
 
 function greyDatesBack(month,year){
   const tempDate = new Date();
@@ -124,4 +125,21 @@ headerspan[1].addEventListener("click", e=>{
   greyDatesBack(monthIndex,year);
 })
 
+var div = document.querySelector(".dates").querySelectorAll("div");
+var i;
+for(i=0;i<42;i++)
+{
+  div[i].style.backgroundColor = colors[0];
+  div[i].addEventListener("click", e=>{
+    console.log(e.target.style.backgroundColor);
+    var j;
+    for( j=0 ; j<7 ; j++){
+      if (e.target.style.backgroundColor == colors[j])
+        break
+    }
+    if(j==6)
+      j=-1;
+    e.target.style.backgroundColor = colors[j+1];
+  })
+}
 
